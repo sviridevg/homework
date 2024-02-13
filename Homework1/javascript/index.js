71a65e5aa499f1e68a43559484da7f08a9f3a597
sviridevg <svirid.evg@yandex.ru>
2024-02-14 00:16:21 +0300

    Exercise 3 + correction Exercise 2

--- Homework1/javascript/index.js
+++ Homework1/javascript/index.js
@@ -6,4 +6,8 @@ alert (`${result}`);
 
 // Exercise 2
 const date = (`2007-06-29`);
-alert (date);
+alert (`Год выпуска первого iPhone ${date}`);
+
+// Exercise 3
+const firstname = `Brendan Eich`;
+alert (`Имя создателя языка JavaScript ${firstname}`);
