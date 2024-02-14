ee2af2f0adc9bc977b8f16e1d29b5d9ffb043dbf
sviridevg <svirid.evg@yandex.ru>
2024-02-14 19:55:53 +0300

    Exercise 4 + correction Exercise 1 + new html for JS hw

--- Homework1/javascript/index.js
+++ Homework1/javascript/index.js
@@ -1,13 +1,27 @@
 // Exercise 1
 let a = 10;
 alert (a);
-let result = a + 20;
-alert (`${result}`);
+a = 20;
+alert (a);
 
 // Exercise 2
 const date = (`2007-06-29`);
-alert (`Год выпуска первого iPhone ${date}`);
+alert (`Дата выпуска первого iPhone ${date}`);
 
 // Exercise 3
 const firstname = `Brendan Eich`;
 alert (`Имя создателя языка JavaScript ${firstname}`);
+
+// Exercise 4
+let x = 10;
+let y = 2;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+alert (`
+    Сумма переменных ${sum}
+    Вычитание переменных ${difference}
+    Умножение переменных ${product}
+    Деление переменных ${quotient}
+`)
