f513d876be125884577adfcb69799d356b4264b1
sviridevg <svirid.evg@yandex.ru>
2024-02-18 18:45:00 +0300

    Exercise 4

--- Homework1/javascript/hw3.js
+++ Homework1/javascript/hw3.js
@@ -6,7 +6,7 @@ if (password == entpass) {
     console.log("Пароль введен верно")
 } else {
     console.log("Пароль введен неправильно")
-}
+};
 
 // Exercise 2
 let c = 8;
@@ -14,7 +14,7 @@ if (c > 0 && c < 10) {
     console.log("Верно")
 } else {
     console.log("Неверно")
-}
+};
 
 // Exercise 3
 let d = 134;
@@ -23,4 +23,10 @@ if (d > 100 || e > 100) {
     console.log("Верно")
 } else {
     console.log("Неверно")
-}
+};
+
+// Exercise 4
+let a = '2';
+let b = '3';
+let sum = Number(a) + Number(b);
+console.log(sum);
