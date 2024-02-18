8b7b2844c4558f81c52a002e0e74d40a08ce3307
sviridevg <svirid.evg@yandex.ru>
2024-02-18 19:23:22 +0300

    Exercise 5

--- Homework1/javascript/hw3.js
+++ Homework1/javascript/hw3.js
@@ -30,3 +30,47 @@ let a = '2';
 let b = '3';
 let sum = Number(a) + Number(b);
 console.log(sum);
+
+// Exercise 5
+let numbmon = String(prompt('Введите номер месяца!'))
+switch (numbmon) {
+    case '12':
+        console.log('Зима');
+        break;
+    case '1':
+        console.log('Зима');
+        break;
+    case '2':
+        console.log('Зима');
+        break;
+    case '3':
+        console.log('Весна');
+        break;
+    case '4':
+        console.log('Весна');
+        break;
+    case '5':
+        console.log('Весна');
+        break;
+    case '6':
+        console.log('Лето');
+        break;
+    case '7':
+        console.log('Лето');
+        break;
+    case '8':
+        console.log('Лето');
+        break;
+    case '9':
+        console.log('Осень');
+        break;
+    case '10':
+        console.log('Осень');
+        break;
+    case '11':
+        console.log('Осень');
+        break;
+    default:
+        console.log('ошибся с номером месяца!');
+        break;
+}
