1b8117327f32988e7e15b3480c4aa93107cbdd72
sviridevg <svirid.evg@yandex.ru>
2024-02-24 14:19:16 +0300

    Exercise 1

---
+++ Homework1/javascript/hw5.js
@@ -0,0 +1,27 @@
+// Exercise 1
+function smallerOfTwoNumbers(a, b) {
+    if (a >= b) {
+        return b;
+    } else {
+        return a;
+    }
+}
+console.log(smallerOfTwoNumbers(8, 4));
+console.log(smallerOfTwoNumbers(6, 6));
+console.log(smallerOfTwoNumbers(1, 6));
+console.log(smallerOfTwoNumbers(9, 12));
+
+
+// Exercise 2
+
+// Exercise 3
+
+// Exercise 4
+
+// Exercise 5
+
+// Exercise 6
+
+// Exercise 7
+
+// Exercise 8
