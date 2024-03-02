414c21655f81e6b313914d789d41697afe27701e
sviridevg <svirid.evg@yandex.ru>
2024-03-02 13:56:02 +0300

    Exercise 1

---
+++ Homework1/javascript/hw6.js
@@ -0,0 +1,34 @@
+// Exercise 1
+const ten = [1, 5, 4, 10, 0, 3];
+for (let endTen = 0; endTen < ten.length; endTen++) {
+    if (ten[endTen] == 10) break
+    console.log(ten[endTen]);
+}
+
+// Exercise 2
+
+// Exercise 3
+
+// Exercise 4
+
+// Exercise 5
+
+// Exercise 6
+
+// Exercise 7
+
+// Exercise 8
+
+// Exercise 9
+
+// Exercise 10
+
+// Exercise 11
+
+// Exercise 12
+
+// Exercise 13
+
+// Exercise 14
+
+// Exercise 15
